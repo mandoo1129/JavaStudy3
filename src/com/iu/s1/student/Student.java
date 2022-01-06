41732ad4b8c718e570948a4e0a5148feda79dc2e
mandoo1129 <97069558+mandoo1129@users.noreply.github.com>
2022-01-06 17:29:35 +0900

    Update Student.java

--- src/com/iu/s1/student/Student.java
+++ src/com/iu/s1/student/Student.java
@@ -2,6 +2,8 @@ package com.iu.s1.student;
 
 public class Student {
 	
+	//Data 저장 용도
+	
 	String name;
 	int num;
 	int kor;
@@ -10,5 +12,21 @@ public class Student {
 	
 	int total;
 	double avg;
+	
+	public void makeTotal() {
+		int total=0;
+		System.out.println("참조변수 this");
+		System.out.println("참조변수 this : "+this);
+		this.total = kor + eng + math;
+		this.avg = this.total/3.0;
+		//평균계산
+		
+	}
+	
+	public void hello() {
+		
+		System.out.println("안녕하세요");
+		
+	}
 
 }
